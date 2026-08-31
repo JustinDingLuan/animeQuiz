04f81c7a4634a8eb210d8bc8e65343eda702e02f
DingLuan He <heralice03280711@gmail.com>
2026-08-31 20:55:40 +0800

    Rename .js to .jsx

--- src/quizPage.js
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import {initQuiz} from './quiz.jsx';
-
-const params = new URLSearchParams(window.location.search);
-// 看 html 的 id
-const questionType = params.get('question-type');
-const questionCount = Number(params.get('question-count'));
-
-initQuiz(questionType, questionCount);
